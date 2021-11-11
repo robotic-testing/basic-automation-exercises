a65251fc0962d5bbb7c6f81c1475325a19aca290
Emilio <emilio2hd@gmail.com>
2021-11-11 22:40:09 +0000

    Add simple login exercise

--- main.rb
+++ main.rb
@@ -9,4 +9,11 @@ class AutomationInPracticeApp < Sinatra::Base
   get '/' do
     erb :home
   end
+
+  get '/simple-login' do
+    @submitted = !params['s'].nil? && !params['s'].empty?
+    @user = params['user'] || {}
+
+    erb :simple_login
+  end
 end
