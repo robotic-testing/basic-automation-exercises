2ae543a90c320743f9b2e4e0c7dba50473698651
Emilio <emilio2hd@gmail.com>
2021-11-13 18:35:47 +0000

    Add test to visit each exercise page

--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -15,4 +15,15 @@ describe 'HomePage' do
 
     expect(exercise_links).not_to be_empty
   end
+
+  it 'visit all exercises pages' do
+    all_link_hrefs = page.all(:css, '[data-testid="exercisesList"] li a').map do |element|
+      element[:href]
+    end
+
+    all_link_hrefs.each do |exercise_url|
+      visit exercise_url
+      expect(page.status_code).to eq(200)
+    end
+  end
 end
