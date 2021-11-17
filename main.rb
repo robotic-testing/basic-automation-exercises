178833f2d644bb9724356eab66a9d3e858405528
Emilio <emilio2hd@gmail.com>
2021-11-17 00:17:58 +0000

    Add username availability exercise

--- main.rb
+++ main.rb
@@ -2,9 +2,12 @@
 
 require 'sinatra'
 require 'sinatra/content_for'
+require 'sinatra/namespace'
+require 'sinatra/json'
 
 class AutomationInPracticeApp < Sinatra::Base
   helpers Sinatra::ContentFor
+  register Sinatra::Namespace
 
   configure do
     set :views, 'app/views'
@@ -28,5 +31,17 @@ class AutomationInPracticeApp < Sinatra::Base
 
   get '/skeletons' do
     erb :skeletons
+  end
+
+  namespace '/username-availability' do
+    get { erb :username_availability }
+
+    get '/check' do
+      username_allowed = !%w[admin superadmin].include?(params['username'])
+
+      sleep 2
+
+      json allowed: username_allowed
+    end
   end
 end
